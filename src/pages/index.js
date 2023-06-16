3fca167b7e468b47666c14b0f4d46b42a397030e
Stanislav-Netreba <stasnetreba1322@gmail.com>
2023-06-17 00:19:27 +0200

    creating mane page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,31 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 function Home() {
     return (
-        <div className="image-container">
-            <Image
-                className="background-mainPage"
-                src="/background-mainPage-photo.png"
-                alt="background"
-                width={'1280'}
-                height={'832'}
-            />
-        </div>
+        <>
+            <div className="image-container">
+                <div className="overlay"></div>
+                <Image
+                    className="background-mainPage"
+                    src="/background-mainPage-photo.png"
+                    alt="background"
+                    width={'1280'}
+                    height={'832'}
+                />
+            </div>
+            <div className="text-container">
+                <h1 className="title-mainpage">
+                    Інвестуй в стартап твого майбутнього
+                </h1>
+                <h4 className="subtitle-mainpage">
+                    Станьте частиною прориву та здійсніть чиюсь мрію.
+                </h4>
+                <button className="invest-button">
+                    <Link href={'/'}>Інвестувати</Link>
+                </button>
+            </div>
+        </>
     );
 }
 
