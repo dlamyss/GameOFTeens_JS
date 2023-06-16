e12fe230f0c0f2be62e68478d6d2589ef1c0ddf8
Stanislav-Netreba <stasnetreba1322@gmail.com>
2023-06-16 23:50:23 +0200

    main-page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import Nav from '@/components/navigation';
 import '@/styles/Nav.css';
 import '@/styles/globals.css';
+import '@/styles/style.css';
 
 export default function App({ Component, pageProps }) {
     return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,17 @@
-function Home() {}
+import Image from 'next/image';
+
+function Home() {
+    return (
+        <div className="image-container">
+            <Image
+                className="background-mainPage"
+                src="/background-mainPage-photo.png"
+                alt="background"
+                width={'1280'}
+                height={'832'}
+            />
+        </div>
+    );
+}
+
 export default Home;
