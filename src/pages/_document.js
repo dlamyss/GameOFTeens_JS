f780adc01234c0158ee2d4063d6b635c6952db26
Stanislav-Netreba <stasnetreba1322@gmail.com>
2023-06-17 03:35:00 +0200

    changed the scroll and added icon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,8 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
+                <link rel="icon" href="/icon.ico" />
+
                 <link
                     href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
                     rel="stylesheet"
